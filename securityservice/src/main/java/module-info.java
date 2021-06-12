9d32bdbd925b425a5bd91ff64bf96915607f6a22
Mete Demirtas <metedemirtas26@gmail.com>
2021-06-12 18:43:04 +0200

    executable pic is added && spotbugs changes

--- securityservice/src/main/java/module-info.java
+++ securityservice/src/main/java/module-info.java
@@ -6,4 +6,7 @@ module com.udacity.catpoint.securityservice.service {
         requires transitive com.udacity.catpoint.imageservice;
         requires transitive java.sql;
         requires miglayout;
+        exports com.udacity.catpoint.securityservice.service;
+        exports com.udacity.catpoint.securityservice.application;
+        exports com.udacity.catpoint.securityservice.data;
 }
