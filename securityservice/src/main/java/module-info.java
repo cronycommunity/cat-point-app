f020bb424d6ea585e42b9088325966046221b90e
Mete Demirtas <metedemirtas26@gmail.com>
2021-06-15 21:23:01 +0200

    add module

---
+++ securityservice/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.udacity.catpoint.securityservice.service {
+    requires transitive java.desktop;
+    requires transitive miglayout.swing;
+    requires transitive java.prefs;
+    requires transitive gson;
+    requires transitive com.google.common;
+    requires transitive com.udacity.catpoint.imageservice;
+    requires transitive java.sql;
+    opens com.udacity.catpoint.securityservice.data to gson;
+    opens com.udacity.catpoint.securityservice.service;
+}
